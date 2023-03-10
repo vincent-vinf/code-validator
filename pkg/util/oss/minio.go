5440f24b722154b0bb168c68a788f5fe73ed350f
陈世龙 <csl01402683@alibaba-inc.com>
2023-03-10 10:35:49 +0800

    feat: oss move

--- pkg/util/oss/minio.go
+++ pkg/util/oss/minio.go
@@ -3,10 +3,13 @@ package oss
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+
 	"github.com/vincent-vinf/code-validator/pkg/util/config"
-	"io"
 )
 
 type Client struct {
@@ -36,6 +39,7 @@ func (c *Client) Get(ctx context.Context, path string) ([]byte, error) {
 		return nil, err
 	}
 	defer object.Close()
+
 	buffer, err := io.ReadAll(object)
 	if err != nil {
 		return nil, err
@@ -48,4 +52,32 @@ func (c *Client) Put(ctx context.Context, path string, data []byte) error {
 	_, err := c.client.PutObject(ctx, c.bucket, path, bytes.NewReader(data), int64(len(data)), minio.PutObjectOptions{ContentType: "text/plain"})
 
 	return err
+}
+
+func (c *Client) Remove(ctx context.Context, path string) error {
+	return c.client.RemoveObject(ctx, c.bucket, path, minio.RemoveObjectOptions{})
+}
+
+func (c *Client) Copy(ctx context.Context, src, dst string) error {
+	_, err := c.client.CopyObject(ctx, minio.CopyDestOptions{
+		Bucket: c.bucket,
+		Object: dst,
+	}, minio.CopySrcOptions{
+		Bucket: c.bucket,
+		Object: src,
+	})
+
+	return err
+}
+
+func (c *Client) Move(ctx context.Context, src, dst string) error {
+	if err := c.Copy(ctx, src, dst); err != nil {
+		return err
+	}
+
+	if err := c.Remove(ctx, src); err != nil {
+		return fmt.Errorf("failed to remove source file, err: %s", err)
+	}
+
+	return nil
 }
